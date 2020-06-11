9ab29cf53fd1920d77d19b6caa8221d082741c36
Shayne Koestler <shayne.koestler@kineticdata.com>
2020-06-11 10:38:08 -0500

    updated component

--- libs/components/src/index.js
+++ libs/components/src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const TestComponent = props => {
   const foo = 2;
   return <div>
-    <h1>Test Component</h1>
+    <h1>Test Component!!!</h1>
     <pre>
       {JSON.stringify(props, null, 2)}
     </pre>
